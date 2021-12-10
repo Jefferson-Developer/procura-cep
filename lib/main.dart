0066904f65ed5e59072a823b6c75875e60b6aef4
JEFFERSON PEDRO DE OLIVEIRA PEREIRA <66677703+Jefferson-Developer@users.noreply.github.com>
2021-12-10 07:02:24 -0300

    Classe main refatorada

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:atividade8/busca_enderecos_page.dart';
+import 'package:atividade8/pages/busca_enderecos.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: BuscaEnderecosPage(),
+    home: BuscaEnderecos(),
   ));
 }
